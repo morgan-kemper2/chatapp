28a4ec555f2e0fcfa0566a5d33d1ff3e236bb41a
Morgan Kemper <morgankemper@Morgans-MacBook-Air.local>
2021-03-18 15:27:52 -0400

    custom actions added

--- components/chat.js
+++ components/chat.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { View, Platform, KeyboardAvoidingView, } from 'react-native';
+import React, { Component } from 'react';
+import { View, Platform, KeyboardAvoidingView, Button, StyleSheet, } from 'react-native';
 import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
 import NetInfo from "@react-native-community/netinfo";
 import AsyncStorage from '@react-native-community/async-storage';
-
+import MapView from 'react-native-maps';
+import CustomActions from './CustomActions';
 
 
 const firebase = require('firebase');
@@ -14,7 +15,7 @@ export default class Chat extends React.Component {
   constructor() {
     super();
 
-    if(!firebase.apps.length) {
+    if (!firebase.apps.length) {
       firebase.initializeApp({
         apiKey: "AIzaSyBilLnaFHgvjwksk3IU-dnucO9_YO1aQIA",
         authDomain: "chat-app-c3861.firebaseapp.com",
@@ -22,14 +23,18 @@ export default class Chat extends React.Component {
         storageBucket: "chat-app-c3861.appspot.com",
         messagingSenderId: "429300359538",
         appId: "1:429300359538:web:d51de51b931e1fdde6bdeb",
-        measurementId: "G-EV3T988YLK"
+        measurementId: "G-EV3T988YLK",
       });
     }
 
     this.referenceChatMessages = firebase.firestore().collection('messages');
 
-    this.state= {
+    this.state = {
       messages: [],
+      image: null,
+      uid: 0,
+      isConnected: false,
+      location: null
     };
   };
 
@@ -65,151 +70,156 @@ export default class Chat extends React.Component {
   };
 
   componentDidMount() {
-    let { name }  = this.props.route.params;
+    let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
 
 
-  NetInfo.fetch().then((state) => {
-    const isConnected = state.isConnected;
-    if (isConnected) {
-      this.setState({
-        isConnected: true,
-      });
 
-      this.authUnsubscribe = firebase
-        .auth()
-        .onAuthStateChanged(async (user) => {
-          if (!user) {
-            await firebase.auth().signInAnonymously();
-          }
+    NetInfo.fetch().then((state) => {
+      const isConnected = state.isConnected;
+      if (isConnected) {
+        this.setState({
+          isConnected: true,
+        });
 
-          this.setState({
-            uid: user.uid,
-            messages: [],
+        this.authUnsubscribe = firebase
+          .auth()
+          .onAuthStateChanged(async (user) => {
+            if (!user) {
+              await firebase.auth().signInAnonymously();
+            }
+
+            this.setState({
+              uid: user.uid,
+              messages: [],
+            });
+
+            this.unsubscribe = this.referenceChatMessages
+              .orderBy("createdAt", "desc")
+              .onSnapshot(this.onCollectionUpdate);
           });
-
-          this.unsubscribe = this.referenceChatMessages
-            .orderBy("createdAt", "desc")
-            .onSnapshot(this.onCollectionUpdate);
+      } else {
+        this.setState({
+          isConnected: false,
         });
-    } else {
-      this.setState({
-        isConnected: false,
+        this.getMessages();
+      }
+    });
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    querySnapshot.forEach((doc) => {
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text || "",
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+        image: data.image || null,
+        location: data.location || null
       });
-      this.getMessages();
-    }
-  });
-}
+    });
+    this.setState({
+      messages,
+    });
+  };
 
-    onCollectionUpdate = (querySnapshot) => {
-      const messages = [];
-      querySnapshot.forEach((doc) => {
-        var data = doc.data(); 
-        messages.push({
-          _id:data._id,
-          text: data.text || "",
-          createdAt: data.createdAt.toDate(),
-          user: data.user,
-        });
-      });
-      this.setState({
-        messages,
-      });
-    };
-
-
-
-    // this.setState({
-    //   messages: [
-    //     {
-    //       _id:1,
-    //       text:'Welcome to the Chat',
-    //       createdAt: new Date(),
-    //       user: {
-    //         _id: 2,
-    //         name: 'React Native',
-    //         avatar: 'https://placeimg.com/140/140/any',
-    //       },
-    //     },
-    //     {
-    //       _id:2,
-    //       text: name + ' has entered the chat', 
-    //       // static message can be anything
-    //       createdAt: new Date(),
-    //       system: true,
-    //     },
-    //   ]
-    // })
-
+  addMessage() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      text: message.text || "",
+      createdAt: message.createdAt,
+      user: message.user,
+      image: message.image || null,
+      location: message.location || null
+    });
+  }
 
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
-}
-
-addMessage() {
-  const message=this.state.messages[0];
-  this.referenceChatMessages.add({
-    _id:message._id,
-    text:message.text,
-    createdAt:message.createdAt,
-    user:message.user
-  });
-}
-
-onSend(messages=[]) {
-  this.setState(
-    (previousState)=>({
-      messages:GiftedChat.append(previousState.messages, messages),
-    }),
-  ()=>{
-    this.addMessage();
-    this.saveMessages();
-    }
-  );
-}
-
-renderInputToolbar = (props) => {
-  if (props.isConnected === false) {
-  } else {
-    return <InputToolbar {...props} />;
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+        this.saveMessages();
+      }
+    );
   }
-};
 
-// changes the color of your speech bubble
-renderBubble(props) {
-  return (
-    <Bubble
-    {...props}
-    wrapperStyle={{
-      right: {
-        backgroundColor:'#000'
-      },
-    }}
-    />
-  );
-}
+  renderInputToolbar = (props) => {
+    if (props.isConnected === false) {
+    } else {
+      return <InputToolbar {...props} />;
+    }
+  };
+
+  // changes the color of your speech bubble
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          },
+        }}
+      />
+    );
+  }
+
+      renderCustomActions = (props) => {
+        return <CustomActions {...props} />;
+      };
+
+      renderCustomView (props) {
+        const { currentMessage} = props;
+        if (currentMessage.location) {
+          return (
+              <MapView
+                style={{width: 150,
+                  height: 100,
+                  borderRadius: 13,
+                  margin: 3}}
+                region={{
+                  latitude: currentMessage.location.latitude,
+                  longitude: currentMessage.location.longitude,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+              />
+          );
+        }
+        return null;
+      }
 
   render() {
-    let {color} = this.props.route.params;
-
+    let { color } = this.props.route.params;
 
     return (
-      <View style={{flex:1, backgroundColor: color}}>
+      <View style={{ flex: 1, backgroundColor: color }}>
         <GiftedChat
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           renderInputToolbar={this.renderInputToolbar}
-          onSend={messages=>this.onSend(messages)}
+          onSend={messages => this.onSend(messages)}
+          renderActions={this.renderCustomActions}
+          renderCustomView={this.renderCustomView}
           user={{
-            _id:1,
+            _id: this.state.uid,
           }}
-          />  
-          {/* keeps android keyboard from hiding your text */}
-          { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}    
-        </View>
+        />
+        {/* keeps android keyboard from hiding your text */}
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+
+      </View>
+
     );
   };
 }
+
+const styles = StyleSheet.create({});
+
 
